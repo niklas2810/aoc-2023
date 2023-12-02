e3e204af9b819a462de1f38bd74b47bd7a6931d2
Niklas Arndt <git@niklasarndt.com>
2023-12-02 11:17:26 +0100

    Improve output and add all target

--- AOC2023/Program.cs
+++ AOC2023/Program.cs
@@ -1,44 +1,74 @@
-﻿using AOC2023.Contracts;
+﻿using AOC2023;
+using AOC2023.Contracts;
 using AOC2023.Days;
 using System.Diagnostics;
 
 var days = new List<DayBase> { new Day01(), new Day02() };
 
-var selected = SelectDay(days);
+if (args != null && args.Length > 0)
+{
+    var runMode = args[0].ToString();
+    if(runMode == "all")
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        foreach(var d in days)
+            ExecuteDay(d);
+        Console.WriteLine(string.Empty.Bordered());
+        Console.WriteLine($"TOTAL TIME: {sw.Elapsed.TotalMilliseconds:00.00}ms");
 
-if(selected == null)
+    } else if(int.TryParse(runMode, out int dayNumber))
+    {
+        var dayFromArgs = SelectDayFromNumber(dayNumber) ?? throw new ArgumentException($"Day {dayNumber} not found!");
+        ExecuteDay(dayFromArgs);
+    } else
+    {
+        Console.WriteLine($"Invalid arguments: '{args}'");
+    }
+    return;
+}
+    
+
+var dayFromPrompt = PromptUserForDay(days);
+
+if(dayFromPrompt == null)
 {
     throw new ArgumentException("No day selected!");
 }
-// Create stopwatch sw and start
-var sw = new Stopwatch();
-sw.Start();
-var input = selected.GenerateInput();
-if(input == null)
+
+ExecuteDay(dayFromPrompt);
+
+void ExecuteDay(DayBase selected)
 {
-    Console.WriteLine("No input generated!");
-    return;
+    Console.WriteLine($"Day {selected.DayNumber:00}: {selected.Name}".Bordered());
+    // Create stopwatch sw and start
+    var sw = new Stopwatch();
+    sw.Start();
+    var input = selected.GenerateInput();
+    if (input == null)
+    {
+        Console.WriteLine("No input generated!");
+        return;
+    }
+    var partOne = selected.SolvePartOne(input);
+    var partTwo = selected.SolvePartTwo(input);
+
+    Console.WriteLine($"PART   ONE: {partOne}");
+    Console.WriteLine($"PART   TWO: {partTwo}");
+
+    // Write elapsed milliseconds with 2 digits precision based on sw.Elapsed.TotalNanoseconds
+    Console.WriteLine($"EXEC  TIME: {sw.Elapsed.TotalMilliseconds:00.00}ms");
 }
-var partOne = selected.SolvePartOne(input);
-var partTwo = selected.SolvePartTwo(input);
 
-Console.WriteLine($"PART  ONE: {partOne}");
-Console.WriteLine($"PART  TWO: {partTwo}");
-
-// Write elapsed milliseconds with 2 digits precision based on sw.Elapsed.TotalNanoseconds
-Console.WriteLine($"EXEC TIME: {sw.Elapsed.TotalMilliseconds:00.00}ms");
-
-DayBase? SelectDay(List<DayBase> days)
+DayBase? PromptUserForDay(List<DayBase> days)
 {
-    if(args != null && args.Length > 0)
-        return SelectDayFromNumber(int.Parse(args[0]));
-
+    Console.WriteLine("SELECT DAY".Bordered());
     foreach (var d in days)
     {
         Console.WriteLine($"[{d.DayNumber:00}] {d.Name}");
     }
 
-    Console.WriteLine("======");
+    Console.WriteLine(string.Empty.Bordered());
     Console.Write("=> ");
     var selectedDay = int.Parse(Console.ReadLine() ?? string.Empty);
     return SelectDayFromNumber(selectedDay);
---
+++ AOC2023/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace AOC2023
+{
+    static class StringExtensions
+    {
+        public static string Bordered(this string s, int targetLength = 50, char borderChar = '=')
+        {
+            if(string.IsNullOrWhiteSpace(s))
+                return new string(borderChar, targetLength);
+
+            var filler = (targetLength - s.Length - 2) / 2;
+
+            // Repeat "=" fillter times
+            var border = new string(borderChar, filler);
+            var fillerChar = ((targetLength-s.Length) % 2) != 0 ? "=" : string.Empty;
+
+            return $"{border} {s} {border}{fillerChar}";
+        }
+    }
+}
