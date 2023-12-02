8bc3d38a9de379ebcc274b407383319f589d4c77
Niklas Arndt <git@niklasarndt.com>
2023-12-02 12:34:10 +0100

    Add example blazor app

---
+++ AOC2023.WebApp/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AOC2023.WebApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress+"/aoc-2023") });
+
+var host = builder.Build().RunAsync();
