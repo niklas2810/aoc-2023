1e54ebd71b2de4e154f7a06a0ad60d841aa8588f
Niklas Arndt <git@niklasarndt.com>
2023-12-02 13:24:43 +0100

    Remove subfolder configuration for webapp

--- AOC2023.WebApp/Program.cs
+++ AOC2023.WebApp/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress+"/aoc-2023") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 var host = builder.Build().RunAsync();
