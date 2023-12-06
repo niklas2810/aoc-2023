92c11b6ca9d5f4dbddf10daff3afc7ad9bc52a4a
Niklas Arndt <git@niklasarndt.com>
2023-12-06 06:28:36 +0100

    Day 6: Wait For It

---
+++ src/libs/AOC2023.Logic/Days/Day06.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace AOC2023.Logic.Days
+{
+	public class Day06 : DayBase
+	{
+		public override string Name => "Wait For It";
+
+		public override Task<long> SolvePartOne(IEnumerable<string> input)
+		{
+			var races = ParseInput(input).ToArray();
+
+			var sum = 1L;
+			foreach((long time, long distance) race in races)
+			{
+				sum *= GetOptions(race.time, race.distance);
+			}
+
+			return Task.FromResult(sum);
+		}
+
+		public override Task<long> SolvePartTwo(IEnumerable<string> input)
+		{
+			(long time, long distance) race = ParseInputTwo(input);
+			var opts = GetOptions(race.time, race.distance);
+			return Task.FromResult(opts);
+		}
+
+		private long GetOptions(long time, long record)
+		{
+			// Time is in milliseconds
+			// Distance is in millimeters
+			// Speed is in millimeters per milliseconds
+			// For every millisecond passed, your speed increases by 1 millimeter per millisecond.
+			for (int mmPerMs = 1; mmPerMs < time; ++mmPerMs)
+			{
+				var timeLeft = time - mmPerMs;
+				var currentDistance = mmPerMs * timeLeft;
+
+
+				if (currentDistance <= record)
+					continue;
+
+				var min = mmPerMs;
+				var max = time - mmPerMs;
+				return max-min+1;
+			}
+
+			return 0L;
+		}
+
+
+		private (long, long) ParseInputTwo(IEnumerable<string> input)
+		{
+			var timeChars = input.ElementAt(0)[10..].ToCharArray().Where(char.IsDigit).ToArray();
+			var time = long.Parse(new string(timeChars));
+
+			var distanceChars = input.ElementAt(1)[10..].ToCharArray().Where(char.IsDigit).ToArray();
+			var distance = long.Parse(new string(distanceChars));
+
+			return (time, distance);
+		}
+
+		private IEnumerable<(long, long)> ParseInput(IEnumerable<string> input)
+		{
+			var times = input.ElementAt(0)[10..].Trim().Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(long.Parse).ToList();
+			var distances = input.ElementAt(1)[10..].Trim().Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(long.Parse).ToList();
+
+			for (int i = 0; i < times.Count; i++)
+				yield return (times[i], distances[i]);
+		}
+	}
+}
---
+++ tests/AOC2023.Tests/Day06Tests.cs
@@ -0,0 +1,73 @@
+﻿using AOC2023.Logic.Days;
+
+namespace AOC2023.Tests
+{
+	[TestClass]
+	public class Day06Tests
+	{
+		private readonly Day06 _day;
+
+		public Day06Tests()
+		{
+			_day = new Day06();
+		}
+
+		[TestMethod]
+		public void TestFileExample()
+		{
+			// Arrange
+			var filename = "example.txt";
+			Assert.IsTrue(File.Exists(_day.GenerateFilePath(filename)), "File path should exist");
+
+			// Act
+			var result = _day.GenerateInput(filename);
+
+			// Assert
+			Assert.AreEqual(2, result.Count(), "2 lines should be read");
+		}
+
+		[TestMethod]
+		public void TestFileInput()
+		{
+			// Arrange
+			var filename = "input.txt";
+			Assert.IsTrue(File.Exists(_day.GenerateFilePath(filename)), "File path should exist");
+
+			// Act
+			var result = _day.GenerateInput(filename);
+
+			// Assert
+			Assert.AreEqual(2, result.Count(), "2 lines should be read");
+		}
+
+		[TestMethod]
+		public async Task TestSolvePartOneExample()
+		{
+			var result = await _day.SolvePartOne(_day.GenerateInput("example.txt"));
+			Assert.AreEqual(288, result, "SolvePartOne should return 288");
+
+		}
+
+		[TestMethod]
+		public async Task TestSolvePartTwoExample()
+		{
+			var result = await _day.SolvePartTwo(_day.GenerateInput("example.txt"));
+			Assert.AreEqual(71503, result, "SolvePartTwo should return 71503");
+		}
+
+		[TestMethod]
+		public async Task TestSolvePartOneActual()
+		{
+			var result = await _day.SolvePartOne(_day.GenerateInput());
+			Assert.AreEqual(1155175, result, "SolvePartOne should return 1155175");
+		}
+
+		[Ignore] // Execution time is too long
+		[TestMethod]
+		public async Task TestSolvePartTwoActual()
+		{
+			var result = await _day.SolvePartTwo(_day.GenerateInput());
+			Assert.AreEqual(35961505, result, "SolvePartTwo should return 35961505");
+		}
+	}
+}
